4c221ef78380e67564d617bd86db7642061dd13e
yoavdror08 <yoavdror2008@gmail.com>
2024-09-15 11:24:26 +0300

    Add files via upload

---
+++ src/BankAccount.java
@@ -0,0 +1,9 @@
+class BankAccount {
+    private int minBalance;
+    private int balance;
+    
+    public BankAccount(int balance, int minBalance) {
+        this.balance = balance;
+        this.minBalance = minBalance;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        BankAccount acct1 = new BankAccount(3000, -1000);
+        System.out.println(acct1.getBalance());
+    }
+}
