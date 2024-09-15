4246d0325f540d34d922aeaaa217bd6e35034fc8
yoavdror08 <yoavdror2008@gmail.com>
2024-09-15 23:22:13 +0300

    Delete src/BankAccount.java

--- src/BankAccount.java
+++
@@ -1,9 +0,0 @@
-class BankAccount {
-    private int minBalance;
-    private int balance;
-    
-    public BankAccount(int balance, int minBalance) {
-        this.balance = balance;
-        this.minBalance = minBalance;
-    }
-}
